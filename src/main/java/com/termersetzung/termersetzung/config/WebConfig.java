132e58efda0a0b00ff33dc876b24deec76893065
Michael Nickel <micha_1997@web.de>
2021-02-14 23:34:28 +0100

    change ldap url

--- src/main/java/com/termersetzung/termersetzung/config/WebConfig.java
+++ src/main/java/com/termersetzung/termersetzung/config/WebConfig.java
@@ -48,7 +48,7 @@ public class WebConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.ldapAuthentication().userDnPatterns("uid={0},ou=people").groupSearchBase("ou=groups").contextSource()
-                .url("ldap://localhost:8389/dc=springframework,dc=org").and().passwordCompare()
+                .url("ldap://durchschlaf.ad.fh-bielefeld.de:8389/dc=springframework,dc=org").and().passwordCompare()
                 .passwordEncoder(new BCryptPasswordEncoder()).passwordAttribute("userPassword");
 
     }
